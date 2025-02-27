497ef54c805d718850ec63b5e3b46a5aeb267658
Anand Sachan <it1729.iiitbhopal@gmail.com>
2025-02-27 20:44:10 +0530

    Checking == operator

--- src/string/LaunchStr1.java
+++ src/string/LaunchStr1.java
@@ -4,8 +4,10 @@ public class LaunchStr1 {
 
 	public static void main(String[] args) {
 		String str = "Anand";
-		String str1 = new String("Sachan");
-		System.out.println(str+" "+str1);
+		String str1 = new String("Anand");
+		String str2 = "Anand";
+		System.out.println(str==str2);
+		System.out.println(str==str1);
 	}
 
 }
