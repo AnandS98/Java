d0ad2e1b245e508a2da93ef70599578a0cf8a6b5
Anand Sachan <it1729.iiitbhopal@gmail.com>
2026-08-30 02:37:45 +0530

    Updated Calculator Code

--- src/oop/classes1.java
+++ src/oop/classes1.java
@@ -1,21 +1,35 @@
 package oop;
 
 class calculator{
-    int a;
-    int b;
 
-    public int add(){
+    public int add(int num1, int num2){
         System.out.print("Add a number: ");
-        return a+b;
+        return num1+num2;
+    }
+
+    public int sub(int num1, int num2){
+        System.out.print("Sub a number: ");
+        return num1-num2;
+    }
+
+    public int mul(int num1, int num2){
+        System.out.print("Mul a number: ");
+        return num1*num2;
+    }
+
+    public int div(int num1, int num2){
+        System.out.print("Div a number: ");
+        return num1/num2;
     }
 }
 
 public class classes1 {
     public static void main(String[] args) {
         calculator calc = new calculator();
-        calc.a=10;
-        calc.b=20;
-        System.out.println(calc.add());
+        System.out.println(calc.add(10,20));
+        System.out.println(calc.sub(20, 10));
+        System.out.println(calc.mul(10,20));
+        System.out.println(calc.div(20, 10));
     }
     
 }
