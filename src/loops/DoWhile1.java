6c8f404e677a1a9bbcd31e03bc5bc648325c6799
Anand Sachan <it1729.iiitbhopal@gmail.com>
2026-08-30 00:52:59 +0530

    Simple Do while loop

---
+++ src/loops/DoWhile1.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class DoWhile1 {
+    public static void main(String[] args) {
+        int i=0;
+        do {
+            System.out.println("Value of i: "+i);
+            i++;
+        }while(i<0);
+    }
+} 
