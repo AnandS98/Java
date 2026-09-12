d22ada094b5860af700cbffc2276118054f079d5
Anand Sachan <it1729.iiitbhopal@gmail.com>
2026-09-13 01:49:52 +0530

    Jagged Array

---
+++ src/array/JaggedArray.java
@@ -0,0 +1,26 @@
+package array;
+
+public class JaggedArray {
+    public static void main(String[] args) {
+        int nums[][] = new int[3][];
+        nums[0] = new int[2];
+        nums[1] = new int[3];
+        nums[2] = new int[4];
+        
+        
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums[i].length; j++){
+                nums[i][j] = (int)(Math.random()*10);
+            }
+        }
+
+        //Using for-each loop
+        for(int[] row: nums){
+            for(int col: row){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
