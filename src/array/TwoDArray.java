e6c1f7b6950a1a880363b0091da0d1dfcdae3417
Anand Sachan <it1729.iiitbhopal@gmail.com>
2026-09-13 01:41:12 +0530

    2 D Array

---
+++ src/array/TwoDArray.java
@@ -0,0 +1,15 @@
+package array;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int nums[][] = new int[3][4];
+        
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums[i].length; j++){
+                nums[i][j] = (int)(Math.random()*10);
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
